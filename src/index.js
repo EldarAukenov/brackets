2c3e7ff61293e1003613822597be99535ff98c18
EldarAukenov <119871117+EldarAukenov@users.noreply.github.com>
2023-02-15 08:43:45 +0600

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,3 +1,68 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+function check(str, bracketsConfig) {
+  const stack = [];
+  for (let i = 0; i < str.length; i++) {
+    let currChar = str[i];
+    let found = false;
+    for (let j = 0; j < bracketsConfig.length; j++) {
+      if (currChar === bracketsConfig[j][0]) {
+        stack.push(bracketsConfig[j][1]);
+        found = true;
+        break;
+      }
+    }
+    if (!found) {
+      let lastInStack = stack.pop();
+      if (currChar !== lastInStack) {
+        return false;
+      }
+    }
+  }
+  return stack.length === 0;
 }
+console.log(check("()", [["(", ")"]])); // -> true
+console.log(check("((()))()", [["(", ")"]])); // -> true
+console.log(check("())(", [["(", ")"]])); // -> false
+console.log(
+  check("([{}])", [
+    ["(", ")"],
+    ["[", "]"],
+    ["{", "}"],
+  ])
+); // -> true
+console.log(
+  check("[(])", [
+    ["(", ")"],
+    ["[", "]"],
+  ])
+); // -> false
+console.log(
+  check("[]()", [
+    ["(", ")"],
+    ["[", "]"],
+  ])
+); // -> true
+console.log(
+  check("[]()(", [
+    ["(", ")"],
+    ["[", "]"],
+  ])
+); // -> false
+console.log(check("||", [["|", "|"]])); // -> true
+console.log(
+  check("|()|", [
+    ["(", ")"],
+    ["|", "|"],
+  ])
+); // -> true
+console.log(
+  check("|(|)", [
+    ["(", ")"],
+    ["|", "|"],
+  ])
+); // -> false
+console.log(
+  check("|()|(||)||", [
+    ["(", ")"],
+    ["|", "|"],
+  ])
+); // -> true
